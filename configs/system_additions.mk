1773d4b3e7591e2a673e7b1f964677d0ffc891c7
Alex Cruz <du.alexcruz@gmail.com>
2018-02-15 19:09:03 -0500

    Fruity pebbles [2/4]
    Change-Id: Ie6e916691449af4b4c3d652d2be6fd9924ac3c8b
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -41,3 +41,27 @@ PRODUCT_PACKAGES += \
     FilesThemeShishuNights \
     WellbeingThemeShishuNights
 
+# Overlays
+PRODUCT_PACKAGES += \
+    AmberAccent \
+    BlackAccent \
+    BlueAccent \
+    BlueGreyAccent \
+    BrownAccent \
+    CyanAccent \
+    DeepOrangeAccent \
+    DeepPurpleAccent \
+    GreenAccent \
+    GreyAccent \
+    IndigoAccent \
+    LightBlueAccent \
+    LightGreenAccent \
+    LimeAccent \
+    OrangeAccent \
+    PinkAccent \
+    PurpleAccent \
+    RedAccent \
+    TealAccent \
+    YellowAccent \
+    WhiteAccent
+
