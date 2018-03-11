9a4a7111892ff915564696d313ce930dab3506bd
doc <doc.divxm@gmail.com>
2018-03-10 19:15:33 -0500

    Fix Miracast on all our devices
    Change-Id: I4d9b2fd5eb06b9099fcd67f3bed139600e1d2353
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/pixeldust_phone.mk
+++ configs/pixeldust_phone.mk
@@ -33,3 +33,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.dun.override=0
 
+# Enable WiFi display
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0
+
