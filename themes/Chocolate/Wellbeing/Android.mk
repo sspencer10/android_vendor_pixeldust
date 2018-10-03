AospExtended/platform_vendor_aosp

Vendor: Overlay wellbeing's settings icon background
 In extended UI and chocolate UI variants we are using custom color foe settings dashboard icons BG,
 use same color for wellbeing icon as well.

Change-Id: I79057b77060a888831f130440cd809d46e29bea0

themes/Chocolate/Wellbeing/Android.mk
@@ -1,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACKAGE_NAME := WellbeingChocolateTheme
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+include $(BUILD_PACKAGE)
+

sspencer10/android_vendor_pixeldust

 In ShishuNights and Chocolate Theme variants we are using custom color
 for settings dashboard icons BG, use same color for wellbeing icon as
 well.
Signed-off-by: spezi77 <spezi7713@gmx.net>

themes/Chocolate/Wellbeing/Android.mk
@@ -1,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACKAGE_NAME := WellbeingThemeChocolate
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+include $(BUILD_PACKAGE)
+