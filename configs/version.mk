96e19e996709bd5d869b690b28be8a21cb1af5c6
spezi77 <spezi7713@gmx.net>
2018-10-08 21:23:06 +0200

    version: It's about time to publish release one.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/version.mk
+++ configs/version.mk
@@ -20,7 +20,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += TARGET_BUILD_TYPE=user
 PD_BUILDTYPE := RELEASE
 
 # Pixel Dust ROM package name 
-PIXELDUST_VERSION := $(TARGET_PRODUCT)-pie-alpha-$(shell date -u +%Y%m%d-%H%M)
+PIXELDUST_VERSION := $(TARGET_PRODUCT)-pie-release-one-$(shell date -u +%Y%m%d-%H%M)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.pixeldust.version=$(BUILD_VERSION)-$(PD_BUILDTYPE) \
