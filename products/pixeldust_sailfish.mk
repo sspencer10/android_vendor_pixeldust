0e8dd9d1dc78400ffad34f65cc7813dafbb06cb3
Steve Spencer <sspencer10@gmail.com>
2018-11-10 21:40:00 -0600

    Update Fingerprint for r16

--- products/pixeldust_sailfish.mk
+++ products/pixeldust_sailfish.mk
@@ -37,15 +37,15 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    PRIVATE_BUILD_DESC="sailfish-user 9 PPR2.181005.003 4984323 release-keys" \
+    PRIVATE_BUILD_DESC="sailfish-user 9 PQ1A.181105.017.A1 5081125 release-keys" \
     BUILD_DISPLAY_ID=Custom_Build_v2.0
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.adb.secure=1
 
 
-BUILD_FINGERPRINT="google/sailfish/sailfish:9/PPR2.181005.003/4984323:user/release-keys"
-BUILD_THUMBPRINT="9/PPR2.181005.003/4984323:user/release-keys"
+BUILD_FINGERPRINT="google/sailfish/sailfish:9/PQ1A.181105.017.A1/5081125:user/release-keys"
+BUILD_THUMBPRINT="9/PQ1A.181105.017.A1/5081125:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.pixeldust.maintainer="spezi77"
